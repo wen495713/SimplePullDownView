e35ac9fa02e73c4ea2cc628d68cab70e826a5a6d
慢吞吞 <ManWaiKam@bunweixintekiMacBook-Pro.local>
2012-08-14 14:27:32 +0800

    Initial Commit

---
+++ PullCurtain/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PullCurtain
+//
+//  Created by 慢吞吞 on 12-8-14.
+//  Copyright (c) 2012年 慢吞吞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PullCurtain/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PullCurtain
+//
+//  Created by 慢吞吞 on 12-8-14.
+//  Copyright (c) 2012年 慢吞吞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
