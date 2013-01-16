21c6dc7f6283aae8b74ab2d6d6fbd687e46e7601
Vincent.Man <Vincent.Wen@achievo.com>
2013-01-16 11:09:18 +0800

    testing added

---
+++ PullCurtain/Testing.h
@@ -0,0 +1,15 @@
+//
+//  Testing.h
+//  PullCurtain
+//
+//  Created by Vincent.Wen on 13-1-16.
+//  Copyright (c) 2013年 慢吞吞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Testing : NSObject
+
+- (void)doSomeTesting;
+
+@end
---
+++ PullCurtain/Testing.m
@@ -0,0 +1,18 @@
+//
+//  Testing.m
+//  PullCurtain
+//
+//  Created by Vincent.Wen on 13-1-16.
+//  Copyright (c) 2013年 慢吞吞. All rights reserved.
+//
+
+#import "Testing.h"
+
+@implementation Testing
+
+- (void)doSomeTesting
+{
+    NSLog(@"test");
+}
+
+@end
